0bf23e565b50d6263c1d3e4d5e4fc4e1b5b789d1
gabriel <flexflit18@gmail.com>
2022-06-30 21:36:16 -0400

    deleted the "main" page and moved all the code to the App.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-import { Main } from './pages/Main'
+import React from 'react'
+import { Route } from "wouter";
+import { Home } from './pages/Home'
+import { SearchResults } from './pages/SearchResults'
+import { Detail } from './pages/Detail'
+import { Context } from './context/StaticContext';
+import { GifsContextProvider } from './context/GifsContext';
+
 function App() {
   return (
     <div className="App">
-      <Main />
+      <Context.Provider
+        value=
+        {
+          {
+            name: 'gabriel',
+            learning: true
+          }
+        }
+      >
+        <GifsContextProvider>
+          <div className="main">
+            <Route
+              component={Home}
+              path="/"
+            />
+            <Route
+              component={SearchResults}
+              path="/search/:keyword"
+            />
+            <Route
+              component={Detail}
+              path="/gif/:id"
+            />
+          </div>
+        </GifsContextProvider>
+      </Context.Provider>
     </div>
   );
 }
