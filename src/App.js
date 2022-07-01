8e77fc8c04aaee8ce1e968c42654271f6d22b5a2
gabriel <flexflit18@gmail.com>
2022-06-30 22:56:39 -0400

    Provider fixed

--- src/App.js
+++ src/App.js
@@ -1,41 +1,50 @@
 import React from 'react'
-import { Route } from "wouter";
+import { Link, Route } from "wouter";
 import { Home } from './pages/Home'
 import { SearchResults } from './pages/SearchResults'
 import { Detail } from './pages/Detail'
 import { Context } from './context/StaticContext';
 import { GifsContextProvider } from './context/GifsContext';
+import logo from './assets/images/logo.png'
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <Context.Provider
-        value=
+    <Context.Provider
+      value=
+      {
         {
-          {
-            name: 'gabriel',
-            learning: true
-          }
+          name: 'gabriel',
+          learning: true
         }
-      >
-        <GifsContextProvider>
-          <div className="main">
-            <Route
-              component={Home}
-              path="/"
-            />
-            <Route
-              component={SearchResults}
-              path="/search/:keyword"
-            />
-            <Route
-              component={Detail}
-              path="/gif/:id"
-            />
-          </div>
-        </GifsContextProvider>
-      </Context.Provider>
-    </div>
+      }
+    >
+      <div className="App">
+        <section className="App__content">
+          <Link to="/">
+            <figure className="App__logo">
+              <img src={logo} alt='gifs to go logo' />
+            </figure>
+          </Link>
+          <GifsContextProvider>
+            <div className="main">
+              <Route
+                component={Home}
+                path="/"
+              />
+              <Route
+                component={SearchResults}
+                path="/search/:keyword"
+              />
+              <Route
+                component={Detail}
+                path="/gif/:id"
+              />
+            </div>
+          </GifsContextProvider>
+        </section>
+      </div>
+    </Context.Provider>
   );
 }
 
